bf7fefc284324874e2ddf7a82cdbb8e93dc863de
Christian Thaler <christian.thaler@tes-dst.com>
2025-03-07 10:44:44 +0100

    switch runlevel to console mode (was demo mode before)

--- include/configs/socfpga_soc64_common.h
+++ include/configs/socfpga_soc64_common.h
@@ -247,7 +247,7 @@ unsigned int cm_get_qspi_controller_clk_hz(void);
 	"smc_fid_rd=0xC2000007\0" \
 	"smc_fid_wr=0xC2000008\0" \
 	"smc_fid_upd=0xC2000009\0 " \
-	"runlevel=5\0" \
+	"runlevel=3\0" \
 	"evalkit=dnx\0"
 #endif /*#if IS_ENABLED(CONFIG_DISTRO_DEFAULTS)*/
 
